928d659e65208fb2d33d869d345c62a67b7a2d8b
davidsaemund <davidsae14@ru.is>
2020-09-03 14:43:11 +0000

    Programm working

--- sequence.py
+++ sequence.py
@@ -1,17 +1,17 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
 #sequence addar síðustu 3 tölum saman og skrifar út sem næstu sumu
-num1 = 1
-prev_num = 0
-prev_num = 0
+num1 = 0
+prev_num = 1
+prev_num1 = 0
 # Start with making a for loop that runs n times 
 for i in range(n):
-    
-    
-    
-  
-# Make the sequence
+    # Make the sequence
+    sum_ = num1 + prev_num + prev_num1
+    prev_num1 = prev_num
+    prev_num = num1
+    num1 = sum_
+    #print out each step
+    print(sum_)
 
-#print out each step
-    print(sequence)
 
