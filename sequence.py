9255732240c6e87d5a4b5e4c33c3cc64a1c3f2b1
davidsaemund <davidsae14@ru.is>
2020-09-03 14:02:27 +0000

    Beinagrind kominn

---
+++ sequence.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+
+# Start with making a for loop that runs n times 
+
+# Make the sequence 
+
+#print out each step
+
