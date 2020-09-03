0a02df15f7a1032865c01fdd966f8965225fb1d1
davidsaemund <davidsae14@ru.is>
2020-09-03 14:29:41 +0000

    Starting to programm

--- sequence.py
+++ sequence.py
@@ -1,9 +1,17 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
-
+#sequence addar síðustu 3 tölum saman og skrifar út sem næstu sumu
+num1 = 1
+prev_num = 0
+prev_num = 0
 # Start with making a for loop that runs n times 
-
-# Make the sequence 
+for i in range(n):
+    
+    
+    
+  
+# Make the sequence
 
 #print out each step
+    print(sequence)
 
